176466d88375c4560808c2ecc162791dc3c977a0
Muhammet Ömer EREN <106866623+p4yz@users.noreply.github.com>
2022-10-22 18:30:31 +0300

    Update ipcheck.sh

--- ipcheck.sh
+++ ipcheck.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -z "$1" ] 
 then
-echo "kullanim : $0 /ipcheck [ip]"
+echo "usage : $0 /ipcheck [ip]"
 exit
 fi
 ipinfo="curl https://ipinfo.io/$1/?token=05490bf99692e1"
-echo "IP Bilgileri :"
+echo "IP information :"
 $ipinfo
 echo ''
